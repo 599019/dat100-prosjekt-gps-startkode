71129794f82aecd608201a25fcbbbef26bb475a2
599019 <111427802+599019@users.noreply.github.com>
2022-10-17 21:49:24 +0200

    Update ShowRoute.java

--- src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
@@ -52,38 +52,61 @@ public class ShowRoute extends EasyGraphics {
 
 	// antall y-pixels per breddegrad
 	public double ystep() {
-	
-		double ystep;
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
 
-		// TODO - SLUTT
-		
+		double maxlat = GPSUtils.findMax(GPSUtils.getLatitudes(gpspoints));
+		double minlat = GPSUtils.findMin(GPSUtils.getLatitudes(gpspoints));
+
+		double ystep = MAPYSIZE / (Math.abs(maxlat - minlat));
+
+		return ystep;
 	}
 
 	public void showRouteMap(int ybase) {
 
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - SLUTT
+		double minlat = GPSUtils.findMin(GPSUtils.getLatitudes(gpspoints));
+		double minlon = GPSUtils.findMin(GPSUtils.getLongitudes(gpspoints));
+		int r = 5;
+
+		setColor(0, 255, 0);
+		for (int i = 0; i < gpspoints.length; i++) {
+			int x1 = (int) (xstep() * (gpspoints[i].getLongitude() - minlon));
+			int y1 = (int) (ystep() * (gpspoints[i].getLatitude() - minlat));
+
+			int x2;
+			int y2;
+
+			if (i < gpspoints.length-1) {
+				x2 = (int) (xstep() * (gpspoints[i + 1].getLongitude() - minlon));
+				y2 = (int) (ystep() * (gpspoints[i + 1].getLatitude() - minlat));
+			} else {
+				x2 = (int) (xstep() * (gpspoints[i].getLongitude() - minlon));
+				y2 = (int) (ystep() * (gpspoints[i].getLatitude() - minlat));
+			}
+
+
+
+			drawLine(MARGIN + x1, ybase - y1, MARGIN + x2, ybase - y2);
+			fillCircle(MARGIN + x1, ybase - y1, r);
+		}
+
 	}
 
 	public void showStatistics() {
 
 		int TEXTDISTANCE = 20;
+		setColor(0, 0, 0);
+		setFont("Courier", 12);
 
-		setColor(0,0,0);
-		setFont("Courier",12);
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - SLUTT;
+		String[] outstr = new String[6];
+		outstr[0] = String.format("%-15s: %8s %n", "Total time", GPSUtils.formatTime(gpscomputer.totalTime()));
+		outstr[1] = String.format("%-15s: %8.2f %-4s %n", "Total distance", gpscomputer.totalDistance() / 1000, "km");
+		outstr[2] = String.format("%-15s: %8.2f %-4s %n", "Total elecation", gpscomputer.totalElevation(), "m");
+		outstr[3] = String.format("%-15s: %8.2f %-4s %n", "Max speed", gpscomputer.maxSpeed(), "km/t");
+		outstr[4] = String.format("%-15s: %8.2f %-4s %n", "Average speed", gpscomputer.averageSpeed(), "km/t");
+		outstr[5] = String.format("%-15s: %8.2f %-4s %n", "Energy", gpscomputer.totalKcal(80), "kcal");
+
+		for (int i = 0; i < outstr.length; i++) {
+			drawString(outstr[i], MARGIN, 50 + TEXTDISTANCE * i);
+		}
 	}
-
 }
